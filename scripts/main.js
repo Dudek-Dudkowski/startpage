c9ca60f72b831da8d1896a3a3bc9013ddeb5ba28
Michał Dudek-Dudkowski <michaldudekdudkowski@gmail.com>
2023-05-13 10:58:46 +0200

    Search bar is now working. It sends the value written by the user to a variable and logs it in the console

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,35 @@
 const searchButton = document.querySelector(`.search-icon`);
-let searchDefault = document.querySelector(`#search`).value = `Szukaj`;
+let searchDefault = (document.querySelector(`#search`).value = `Szukaj`);
 let searchInput = document.querySelector(`#search`);
 
-searchInput.addEventListener('focus', function(){
-    if(searchDefault==='Szukaj'){
-        searchInput.value = '';
-    }
-    // else{
-    //     searchInput.value = 'Szukaj';
-    // }
+searchInput.addEventListener("focus", function () {
+	if (searchDefault === "Szukaj") {
+		searchInput.value = "";
+	}
 });
 
-searchInput.addEventListener('blur', function(){
-    searchInput.value = 'Szukaj';
-        // searchInput.value = 'Szukaj';
-  
+searchInput.addEventListener("blur", function () {
+	searchInput.value = "Szukaj";
+	// searchInput.value = 'Szukaj';
 });
 
-
-searchButton.addEventListener("click", function(){
-    console.log(searchInput); 
+searchButton.addEventListener("click", function () {
+	console.log(searchInput);
 });
+
+searchInput.addEventListener("keydown", function () {
+	let key = event.key;
+
+	if (key === "Enter") {
+		console.log(searchInput.value);
+	}
+});
+
+// let input = document.querySelector('#myInput');
+
+// input.addEventListener('keydown', function(event) {
+//   let key = event.key;
+//   if (key === 'Enter') {
+//     // tutaj wpisz kod do przetworzenia zawartości inputa
+//   }
+// });

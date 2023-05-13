749dc7e42f8fbf618cc2bc6f32b71acc442e2ea3
Michał Dudek-Dudkowski <michaldudekdudkowski@gmail.com>
2023-05-13 11:52:20 +0200

    String written in the form is now saved in the array in the correct order.

--- scripts/main.js
+++ scripts/main.js
@@ -29,13 +29,33 @@ searchInput.addEventListener("keydown", function () {
 		let arrayLenght = inputArray.length;
 		console.log(arrayLenght);
 
-        // do {
-        //     // kod do wykonania
-        //   } while (warunek);
+        inputArray.reverse();
+       
+        let arrayMirror = [];
+
+        while(arrayLenght>=1){
+            arrayLenght--;
+            // console.log(arrayLenght);
+            console.log(inputArray[arrayLenght]);
+            
+            
+        }
 
 	}
 });
 
-// let inputString = "To jest przykładowy tekst";
-// let inputArray = inputString.split(" ");
-// console.log(inputArray); // ["To", "jest", "przykładowy", "tekst"]
+// const array1 = [1, 2, 3, 4];
+// const array2 = [];
+
+// for (let i = array1.length - 1; i >= 0; i--) {
+//   array2.push(array1[i]);
+// }
+
+// console.log(array2); // [4, 3, 2, 1]
+
+
+
+// for (let i = inputArray.length - 1; i >= 0; i--){
+//     arrayMirror.push(arrayLenght[i]);
+//     console.log(arrayMirror);
+// }
